1e0cd9b1a97672ffdfdb6036bdfb00d0f7fd596f
kybex11 <artm.22@internet.ru>
2024-09-12 22:44:19 +0000

    check hiughlight

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import { runEditorWithFile, runEditorWithoutFile } from './modules/editor.js';
 import { exitWithEsc } from './modules/tools.js';
+import { Highlight } from './modules/highlight.js';
 
 const filePath = process.argv[2];
 
@@ -20,4 +21,15 @@ function checkFile() {
   }
 }
 
-checkFile();
+//checkFile();
+
+const highlight = new Highlight();
+console.log(highlight.HighlightCode(`
+  import * as fs from 'fs';
+
+  function main() {
+    console.log("hello");
+  }
+
+  main();
+  `, 'ts'));
