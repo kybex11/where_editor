e717b93cc2cdc8e5d60a469243979b4d8056f862
kybex11 <artm.22@internet.ru>
2024-09-12 09:48:52 +0000

    add SyntaxHighlight for Typescript && JavaScript

--- src/modules/highlight.ts
+++ src/modules/highlight.ts
@@ -2,12 +2,24 @@ import chalk from "chalk";
 
 export class Highlight {
     HighlightOperators(content: any) {
-        const operators = ['{', '}', '[', ']', '+', '-', '&', '*', '!', '?', '=', '(', ')', '#', '%', ';', ':', '|'];
+        const operators = ['{', '}', '[', ']', '+', '-', '&', '*', '!', '?', '=', '(', ')', '#', '%', ';', ':', '|', '>', '<', ',', '.'];
 
         const highlightedText = content.replace(new RegExp(`(${operators.join('|')})`, 'gi'), (match) => {
             return chalk.blue(match);
         })
 
         return highlightedText;
+    }
+
+    HighlightCode(content: any, lang: string) {
+        if (lang == "ts" || lang == "js") {
+        const operators = ['let', 'const', 'var', 'function', 'delete', 'void', 'public', 'private', 'generator', 'async', 'await', 'class', 'type', 'interface', 'enum', 'module', 'namespace', 'import', 'export', 'default', 'abstract', 'any', 'as', 'boolean', 'break', 'case', 'switch', 'catch', 'continue', 'debugger', 'declare', 'do', 'else', 'if', 'for', 'while', 'extends', 'false', 'finally', 'from', 'get', 'global', 'implements', 'infer', 'instanceof', 'never', 'new', 'null', 'number', 'object', 'if', 'protected', 'readonly', 'require', 'return', 'string', 'super', 'symbol', 'this', 'throw', 'true', 'try', 'type', 'typeof', 'with', 'yield', 'undefined'];
+
+        const highlightedText = content.replace(new RegExp(`(${operators.join('|')})`, 'gi'), (match) => {
+            return chalk.red(match);
+        })
+
+        return highlightedText;
+        }
     }
 }
